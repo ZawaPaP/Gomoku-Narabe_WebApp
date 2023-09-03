a3700874ea936aac4df0f311256e262500791031
Kazu <kazuhideizawa@gmail.com>
2023-09-03 23:01:10 +0800

    removed old file

--- back-end/config/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("gomoku-narabe/", include("gomokunarabe.urls")), 
-    path("admin/", admin.site.urls),
-]
